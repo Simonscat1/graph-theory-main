ff60fc3082ff8d7047a536bd5c6e5c3fa62ff78e
Simon <kazakeev_roman@mail.ru>
2023-12-25 11:15:24 +0800

    1

--- Муравейный алгоритм/main.py
+++ Муравейный алгоритм/main.py
@@ -1,15 +1,8 @@
 import random
 
 class ant_algorithm:
-    def __init__(self, graphLen, graph, num_iterations):
-        self.graphLen = graphLen
-        self.graph = graph
-        self.num_iterations = num_iterations
-
-    def next_sity(self, current_city, visited_cities, pheromone):
-        cities = set(c for c, _ in self.graph[current_city]) - set(visited_cities)
-        if not cities:
-            return random.choice(list(set(self.graph.keys()) - set(visited_cities)))
+    def select_next_city(self, graph, current_city, visited_cities, pheromone):
+        cities = set(c for c, _ in graph[current_city])
         probabilities = []
         for city in cities:
             numerator = pheromone[(current_city, city)]
@@ -20,62 +13,77 @@ class ant_algorithm:
             probabilities.append(probability)
         return random.choices(list(cities), probabilities)[0]
 
-    def updatePheromones(self, pheromone, allPaths, decay=0.1):
+
+
+    def update_pheromones(self, pheromone, all_paths, decay=0.1):
         for k, v in pheromone.items():
             pheromone[k] = v * (1.0 - decay)
-        for path, cost in allPaths:
+        for path, cost in all_paths:
             for i in range(len(path) - 1):
                 if (path[i], path[i + 1]) in pheromone and cost != 0:
                     pheromone[(path[i], path[i + 1])] += 1.0 / cost
         return pheromone
 
-    def pathCost(self, path):
-        if(len(path) <2):
+
+    def path_cost(self, path):
+        if len(path) < 2:
             return 0
-        cost = 0
+        total_cost = 0
         for i in range(1, len(path)):
-            found = False
-            for city, cost in self.graph[path[i - 1]]:
+            cost_found = False
+            for city, cost in graph[path[i - 1]]:
                 if city == path[i]:
-                    cost += cost
-                    found = True
+                    total_cost += cost
+                    cost_found = True
                     break
-            if not found:
-                cost += 0
-        return cost
+            if not cost_found:
+                total_cost += 0
+        return total_cost
 
-    def algorithm(self):
+    def algorithm(self, graph, num_ants, num_iterations):
         pheromone = {}
-        for city in self.graph:
-            for city1, _ in graph[city]:
-                pheromone[(city,city1)] = 1
-        allPath = []
+        alpha_ants = 0
+        max_pheromone = 0
+        for city1 in graph:
+            for city2, _ in graph[city1]:
+                pheromone[(city1, city2)] = 1
+        best_path = None
+        all_paths = []
 
-        for i in range(self.num_iterations):
-            for _ in range(self.graphLen):
+        for _ in range(num_iterations):
+            for _ in range(num_ants):
                 path = [random.choice(list(graph.keys()))]
-                while len(path) < self.graphLen:
-                    sity = self.next_sity(path[-1], path, pheromone)
-                    if sity is None:
+                total_pheromone = 0
+                while len(path) < len(graph):
+                    next_city = self.select_next_city(graph, path[-1], path, pheromone)
+                    if next_city is None:
                         break
-                    path.append(sity)
-                if(path[-1], path[0]) not in pheromone:
+                    path.append(next_city)
+                    total_pheromone += pheromone[(path[-2], path[-1])]
+                if (path[-1], path[0]) not in pheromone:
                     continue
-                allPath.append((path, self.pathCost(path)))
-            pheromone = self.updatePheromones(pheromone, allPath)
-        return min(allPath, key=lambda x: x[1])
+                all_paths.append((path, self.path_cost(path)))
+                if total_pheromone > max_pheromone:
+                    max_pheromone = total_pheromone
+                    alpha_ants += 1
+            pheromone = self.update_pheromones(pheromone, all_paths)
+
+        print(f"Количество альфа-муравьев: {alpha_ants}")
+        return min(all_paths, key=lambda x: x[1])
+
+
 
 graph = {
     1: [(2, 10), (3, 6), (4, 8)],
-    2: [(4, 5), (7, 11)],
-    3: [(5, 3)],
-    4: [(3, 2), (5, 5), (6, 7), (7, 12)],
-    5: [(6, 9), (9, 12)],
-    6: [(8, 8), (9, 10)],
-    7: [(6, 4), (8, 6)],
-    8: [(9, 1)],
-    9: []
+    2: [(4, 5), (7, 11), (1, 3)],
+    3: [(5, 3), (1, 4)],
+    4: [(3, 2), (5, 5), (6, 7), (7, 12), (1, 2)],
+    5: [(6, 9), (9, 12), (1, 1)],
+    6: [(8, 8), (9, 10), (1, 6)],
+    7: [(6, 4), (8, 6), (1, 7)],
+    8: [(9, 1), (1, 8)],
+    9: [(1, 9)]
 }
+s = ant_algorithm()
+print(s.algorithm(graph, len(graph), 1000))
 
-s = ant_algorithm(len(graph), graph, 1000)
-print(s.algorithm())
